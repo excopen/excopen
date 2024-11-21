1e9fe9f12ad3f34887e2d364823a79b9b3c0f83e
MarkLevanovich999 <dalastexas999@gmail.com>
2024-11-21 18:32:56 +0600

    Update Footer

--- client/src/widgets/footer/ui/Index.tsx
+++ client/src/widgets/footer/ui/Index.tsx
@@ -1,9 +1,22 @@
 import {FC} from "react";
+import {Navigation} from "@/widgets/footer/ui/Navigation.tsx";
+import {Info} from "@/widgets/footer/ui/Info.tsx";
+import {Separator} from "@/widgets/footer/ui/Separator.tsx";
+import {Annotation} from "@/widgets/footer/ui/Annotation.tsx";
+import {Support} from "@/widgets/footer/ui/Support.tsx";
+import {InfoContainer} from "@/widgets/footer/ui/InfoContainer.tsx";
+import {FooterContainer} from "@/widgets/footer/ui/FooterContainer.tsx";
 
 export const Footer: FC = () => {
     return (
-        <footer>
-            FOOTER
-        </footer>
+        <FooterContainer>
+            <InfoContainer>
+                <Navigation/>
+                <Info/>
+            </InfoContainer>
+            <Separator/>
+            <Annotation/>
+            <Support/>
+        </FooterContainer>
     );
 };
---
+++ client/src/widgets/footer/ui/Separator.tsx
@@ -0,0 +1,5 @@
+import {FC} from "react";
+
+export const Separator: FC = () => {
+    return <hr className={"h-px bg-grayscale-300 my-4 px-4 md:px-32 lg:px-56"}/>
+};
