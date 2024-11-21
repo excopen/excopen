679da5d8c8927f402938d675934a5f0702ad4c56
MarkLevanovich999 <dalastexas999@gmail.com>
2024-11-21 21:27:30 +0600

    Update label in breadcrumbs

--- client/src/features/breadcrumbs/model/hooks.ts
+++ client/src/features/breadcrumbs/model/hooks.ts
@@ -2,22 +2,26 @@ import {useLocation} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {RouteNames} from "@/shared/types";
 import {BreadcrumbType} from "./types.ts";
+import {routesConfig} from "@/features/breadcrumbs/utils";
 
 export const useBreadcrumbs = (): BreadcrumbType[] => {
 
     const location = useLocation();
-    const [breadcrumbs, setBreadcrumbs] = useState<BreadcrumbType[]>([{ path: `/${RouteNames.MAIN}`, label: 'главная' }]);
+    const [breadcrumbs, setBreadcrumbs] = useState<BreadcrumbType[]>([{ path: `/${RouteNames.MAIN}`, label: 'Главная' }]);
 
     useEffect(() => {
 
         const path: string[] = location.pathname.split('/').filter(Boolean);
         const currentPath: string = `/${path.join('/')}`;
 
+        const currentRoute = routesConfig.find(route => route.path === currentPath)
+        const currentLabel = currentRoute ? currentRoute.label : path[path.length - 1] || RouteNames.MAIN
+
         if (!breadcrumbs.find(breadcrumb => breadcrumb.path === currentPath)) {
             setBreadcrumbs(
                 prev => [
                     ...prev,
-                    {path: currentPath, label: path[path.length - 1] || RouteNames.MAIN}
+                    {path: currentPath, label: currentLabel}
                 ]
             );
         }
