a3ab7efed1b08e3cafc32d79bdee3765db93f278
KromSanya <kromval6@gmail.com>
2024-11-28 20:11:46 +0600

    fix(security): resolve issues with authorization configuration
    - Updated `app.properties` with proper settings for OAuth2 and JWT integration.
    - Adjusted `SecurityConfig` to include a `JwtDecoder` and a `JwtAuthenticationConverter` for improved JWT token handling.
    - Added the dependency `implementation 'org.springframework.boot:spring-boot-starter-oauth2-resource-server'` in `build.gradle` to enable support for the OAuth2 Resource Server.

--- src/main/java/excopen/backend/security/SecurityConfig.java
+++ src/main/java/excopen/backend/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package excopen.backend.security;
 
-
 import excopen.backend.servicesImpl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     private final UserServiceImpl customOAuth2UserService;
 
@@ -28,9 +34,22 @@ public class SecurityConfig {
                                 userInfo.userService(customOAuth2UserService)
                         )
                 )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/").permitAll()
                 );
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
+
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
     }
 }
