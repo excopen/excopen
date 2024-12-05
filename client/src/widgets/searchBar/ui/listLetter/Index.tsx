602439858087fb77c8e12472371f0f4cf9834e4d
MarkLevanovich999 <dalastexas999@gmail.com>
2024-12-05 21:41:21 +0600

    Add widget search location bar

---
+++ client/src/widgets/searchBar/ui/listLetter/Index.tsx
@@ -0,0 +1,37 @@
+import { FC, useEffect, useState } from "react";
+import style from "./style.module.css";
+import { cn } from "@/app/lib/utils.ts";
+import { lettersArray } from "@/widgets/searchBar/utils";
+
+type IndexProps = {
+    onChange: (value: string) => void
+    isActive: boolean
+};
+
+export const Index: FC<IndexProps> = ({ onChange, isActive }) => {
+
+    const [value, setValue] = useState<string>("А");
+
+    const clickHandler = (newValue: string) => setValue(newValue)
+
+    useEffect(() => {
+        onChange(value);
+    }, [onChange, value]);
+
+    return (
+        <div className={style.container}>
+            {lettersArray.map((letter) => (
+                <span
+                    key={letter}
+                    onClick={() => clickHandler(letter)}
+                    className={cn(
+                        value === letter && !isActive ? style.active : style.letter,
+                        letter !== "Я" && style.border
+                    )}
+                >
+                    {letter}
+                </span>
+            ))}
+        </div>
+    );
+};
