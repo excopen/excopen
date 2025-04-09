a2f0a44c3af817ff5ca4703988340c5adeaad69f
MarkLevanovich999 <dalastexas999@gmail.com>
2025-04-09 11:28:28 +0600

    fix map view ui

--- client/src/entities/tour/ui/carousel/Index.tsx
+++ client/src/entities/tour/ui/carousel/Index.tsx
@@ -1,6 +1,5 @@
 import {FC} from "react";
 import {ImagesCarousel, MapView} from "@/shared/ui";
-import {Card} from "@/shared/ui/card";
 import {YandexMapCoordinates} from "@/shared/types";
 
 type CarouselProps = {
@@ -10,10 +9,8 @@ type CarouselProps = {
 
 export const Index: FC<CarouselProps> = ({images, coordinates}) => {
     return (
-        <div className={"flex flex-col max-lg:items-center gap-4 lg:flex-row my-8"}>
-            <Card className={"h-full"}>
-                <MapView value={coordinates}/>
-            </Card>
+        <div className={"flex flex-col gap-4 wide:flex-row my-8"}>
+            <MapView value={coordinates}/>
             <ImagesCarousel images={images}/>
         </div>
     );
--- client/src/shared/ui/map/MapView.tsx
+++ client/src/shared/ui/map/MapView.tsx
@@ -2,17 +2,18 @@ import {FC} from "react";
 import { YandexMapCoordinates } from "@/shared/types";
 import {Placemark, YMaps, Map} from "@pbe/react-yandex-maps";
 import {ApiKeys} from "@/app/config.ts";
-import {CardMap} from "@/shared/ui";
+import {cn} from "@/app/lib";
 
 type MapProps = {
     value: YandexMapCoordinates
 }
 
-export const MapView: FC<MapProps> = ({ value }) => {
+export const MapView: FC<MapProps> = ({value }) => {
     return (
         <YMaps query={{apikey: ApiKeys.YANDEX_MAP_API}}>
-            <CardMap>
+            <div className={cn("w-full wide:w-[420px] rounded-2xl overflow-hidden")}>
                 <Map
+                    width={"100%"}
                     state={{
                         center: [value.point.latitude, value.point.longitude],
                         zoom: value.zoom,
@@ -21,7 +22,7 @@ export const MapView: FC<MapProps> = ({ value }) => {
                 >
                     <Placemark geometry={[value.point.latitude, value.point.longitude]}/>
                 </Map>
-            </CardMap>
+            </div>
         </YMaps>
     );
 };
