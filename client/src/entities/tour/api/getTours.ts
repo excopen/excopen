080df69cb56c035ec7086edaea82962009f927f5
MarkLevanovich999 <dalastexas999@gmail.com>
2025-05-09 13:24:25 +0600

    update search tour

--- client/src/entities/tour/api/getTours.ts
+++ client/src/entities/tour/api/getTours.ts
@@ -8,17 +8,16 @@ type SearchTourParamsType = {
 }
 
 export const getTours = async (searchTourParams: SearchTourParamsType): Promise<ITour[]> => {
-
     try {
 
         const { date, location, accessibility, byCity } = searchTourParams.searchParams;
 
         const params = {
             "_sort": searchTourParams.sort,
-            "location.city": location.city,
-            "location.region": location.region,
-            "date.from": date.from?.toISOString(),
-            "date.to": date.to?.toISOString(),
+            "city": location.city,
+            "region": location.region,
+            "from": date.from?.toString().split("T")[0] || " ",
+            "to": date.to?.toString().split("T")[0] || " ",
             "accessibility": accessibility,
             "byCity": byCity,
         }
@@ -29,6 +28,7 @@ export const getTours = async (searchTourParams: SearchTourParamsType): Promise<
         return response.data
 
     } catch (e) {
+        console.log("Tours not found.")
         if (isAxiosError(e)) {
             throw new ApiException<ITour>(e.message, e.response?.status, e.response?.data as ITour[] | undefined)
         }
--- client/src/entities/tour/model/useTours.ts
+++ client/src/entities/tour/model/useTours.ts
@@ -3,24 +3,20 @@ import {useQuery, useQueryClient} from "@tanstack/react-query";
 import {ApiException} from "@/shared/lib";
 import {getTours} from "@/entities/tour/api";
 import {searchTourStore as store} from "@/features";
-import {useMemo} from "react";
-import {toJS} from "mobx";
 
 export const useTours = () => {
 
     const queryClient = useQueryClient()
-    const searchParams = useMemo(() => toJS(store.searchParams), [store.searchParams])
 
     const query = useQuery<ITour[], ApiException<ITour>>({
-        queryKey: ["tours", searchParams],
-        queryFn: () => getTours(searchParams),
-        staleTime: 10_000,
-        refetchOnWindowFocus: true,
+        queryKey: ["tours", store.searchParams],
+        queryFn: () => getTours(store.searchParams),
+        staleTime: 60_000,
         placeholderData: []
     })
 
     const clearCache = async () => {
-        await queryClient.invalidateQueries({queryKey: ["tours", searchParams]})
+        await queryClient.invalidateQueries({queryKey: ["tours", store.searchParams]})
     }
 
     return {
