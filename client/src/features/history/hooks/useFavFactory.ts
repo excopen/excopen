715b463701d774c812bd36faaf3a8528e6fd2b63
MarkLevanovich999 <dalastexas999@gmail.com>
2025-05-24 18:27:34 +0600

    update favourites

--- client/src/features/history/hooks/useFavFactory.ts
+++ client/src/features/history/hooks/useFavFactory.ts
@@ -1,14 +1,35 @@
-import {useMemo} from "react";
+import {useEffect, useMemo} from "react";
 import {ITour} from "@/shared/types";
-import {tourLocalHistoryStore as history, authStore as auth, useFavourites} from "@/features";
+import {
+    tourLocalHistoryStore as history,
+    authStore as auth,
+    useFavourites, useAddManyToFavourites
+} from "@/features";
 
-export const useFavFactory = () : ITour[] => {
+type ReturnType = {
+    data: ITour[]
+    isPending: boolean
+    isLoading: boolean
+}
+
+export const useFavFactory = () : ReturnType => {
 
     const isAuth = auth.isAuth
-
-    const {safeData} = useFavourites(isAuth)
     const local = history.favourites
+    const localIds: number[] = local.map(tour => tour.id)
 
-    return useMemo(() => isAuth ? safeData : local, [safeData, isAuth, local])
+    const {safeData: serverData, isLoading} = useFavourites(isAuth)
+    const {mutate: sync, isPending} = useAddManyToFavourites()
+
+    useEffect(() => {
+        if (isAuth && local.length > 0) sync(localIds)
+    }, [])
+
+    const data = useMemo(() => {
+        if (isAuth) return serverData
+        return local
+    }, [serverData, isAuth, local])
+
+    return { data, isLoading, isPending }
 
 }
